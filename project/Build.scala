1659624cef20b7ead897049305009874a25c2cf3
markoboger <marko.boger@gmail.com>
2013-04-03 22:47:34 +0200

    changed group id in Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -14,7 +14,7 @@ object ApplicationBuild extends Build {
     javaEbean,
     "com.google.inject" % "guice" % "3.0",
     "com.google.inject.extensions" % "guice-multibindings" % "3.0",
-    "util" % "util_2.10" % "1.0-SNAPSHOT",
+    "de.htwg" % "util" % "1.0-SNAPSHOT",
     "de.htwg.seapal" % "boatdemo" % "1.3-SNAPSHOT" ,
     "de.htwg.seapal" % "persondemo" % "1.1-SNAPSHOT" 
   )
